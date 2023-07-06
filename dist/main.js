dab2295f56abe2e2d59c869f0297ca6cf3159c55
demstar16 <44187160+demstar16@users.noreply.github.com>
2023-07-06 14:38:32 +0800

    completed skills page

--- dist/main.js
+++ dist/main.js
@@ -14,33 +14,6 @@ let quotes = [
     "Never let the fear of striking out keep you from playing the game.",
     "One day at a time."
 ];
-let languages = [
-    ["../images/python.png", "Python"],
-    ["../images/css.png", "CSS"],
-    ["../images/html.png", "HTML"],
-    ["../images/js.png", "JavaScript"],
-    ["../images/ts.png", "TypeScript"],
-    ["../images/c.png", "C"],
-    ["../images/java.png", "Java"],
-    ["../images/git.png", "Git"]
-];
-let frameworks = [
-    ["../images/django.png", "Django"],
-    ["../images/flask.png", "Flask"],
-    ["../images/nodejs.png", "NodeJS"],
-    ["../images/electron.png", "ElectronJS"]
-];
-let software = [
-    ["../images/myob.png", "MYOB"],
-    ["../images/excel.png", "Microsoft Excel"],
-    ["../images/githubdesktop.png", "GitHub Desktop"],
-    ["../images/docker.png", "Docker"],
-];
-let os = [
-    ["../images/windows.png", "Windows"],
-    ["../images/mac.png", "Macintosh"],
-    ["../images/linux.png", "Linux"]
-];
 document.querySelectorAll(".carousel").forEach(carousel => {
     const items = carousel.querySelectorAll(".carousel-item");
     const buttonsHTML = Array.from(items, () => {
@@ -80,39 +53,13 @@ function quoteGenerator() {
     }
 }
 function iconReveal(elementId) {
-    const element = document.getElementById(elementId);
-    if (element) {
-        if (elementId == "languages") {
-            for (let i = 0; i < languages.length; i++) {
-                let icon = document.createElement("img");
-                icon.setAttribute("src", languages[i][0]);
-                icon.setAttribute("alt", languages[i][1]);
-                element.appendChild(icon);
-            }
+    const div = document.getElementById(elementId);
+    if (div) {
+        if (div.style.display == "none") {
+            div.style.display = "flex";
         }
-        else if (elementId == "frameworks") {
-            for (let i = 0; i < frameworks.length; i++) {
-                let icon = document.createElement("img");
-                icon.setAttribute("src", frameworks[i][0]);
-                icon.setAttribute("alt", frameworks[i][1]);
-                element.appendChild(icon);
-            }
-        }
-        else if (elementId == "software") {
-            for (let i = 0; i < software.length; i++) {
-                let icon = document.createElement("img");
-                icon.setAttribute("src", software[i][0]);
-                icon.setAttribute("alt", software[i][1]);
-                element.appendChild(icon);
-            }
-        }
-        else if (elementId == "os") {
-            for (let i = 0; i < os.length; i++) {
-                let icon = document.createElement("img");
-                icon.setAttribute("src", os[i][0]);
-                icon.setAttribute("alt", os[i][1]);
-                element.appendChild(icon);
-            }
+        else {
+            div.style.display = "none";
         }
     }
 }
