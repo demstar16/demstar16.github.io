94665699a7be37933504de624a4833aca385c0fb
demstar16 <44187160+demstar16@users.noreply.github.com>
2023-10-15 11:57:01 +0800

    changed icons for a cleaner look

--- js/projects.js
+++ js/projects.js
@@ -3,51 +3,57 @@ document.addEventListener("DOMContentLoaded", function() {
     let projectData = [
         [
             'Cleaning Equipment WA', 
-            '../images/cewa-site.png', 
+            '../images/cewa-no-slogan.png', 
             'A web application written in Django and deployed on Digital Ocean developed by myself for my family business, Cleaning Equipment WA.', 
             'https://cewa.d-sons.com/'
         ],
         [
+            'Password Manager', 
+            '../images/password-manager.png',
+            'A python script that uses a csv file to store and manage tough to crack passwords. Although not eniterly secure, it was a fun project to play around with.',
+            'https://github.com/demstar16/password-manager'
+        ],
+        [
             'Pong Clone', 
-            '../images/pong.png', 
+            '../images/pong1.png', 
             'A Pong clone created in the Harvard CS50 gaming course written in Lua. I did not create the code, I just extended it to include a basic AI you could play along with the 2 player.',
-            'https://github.com/demstar16/cs50-pong'
+            'https://github.com/demstar16/pong-clone'
+        ],
+        [
+            'rake', 
+            '../images/rake1.png', 
+            'A client and server applications for rake, a protocol for distributed compilation and remote command execution. Completed in a pair using Python and C.',
+            'https://github.com/pavo-etc/rake'
+        ],
+        [
+            'duplicates', 
+            '../images/duplicates1.png', 
+            'duplicates is a command line utility to locate and report duplicate files in a named directory based on SHA2 hashes. I completed this project with my project partner for the Systems Programming unit.',
+            'https://github.com/demstar16/cli-duplicates'
+        ],
+        [
+            'Ridgefield Farm Database', 
+            '../images/ridgefield1.png',
+            'A web application I designed and developed in a team of 4, working closely with the client, for Ridgefield Farm UWA to help them manage paddock data. Built using Django and deployed on Heroku.',
+            'https://github.com/demstar16/ridgefield-farm-database'
         ],
         [
             'Flappy Bird Clone', 
             '../images/flappy-bird.png', 
             'A Flappy Bird clone created in the Harvard CS50 gaming course written in Lua. I did not create all the code but rather added features, these included: pause functionality, rewards depending on score, and randomizing distance between pipe pairs and gap distance.',
-            'https://github.com/demstar16/cs50-flappy-bird'
+            'https://github.com/demstar16/flappy-bird-clone'
         ],
         [
             'Triumph', 
             '../images/triumph.png', 
             'A terminal political war game developed with Python using the numpy, networkx and matplotx libraries. Bayesian equations are integrated in to make the AI aspects of the game make better decisions.',
-            'https://github.com/demstar16/Triumph'
+            'https://github.com/demstar16/triumph'
         ],
         [
             'ExamGPT', 
             '../images/examgpt.png', 
             'A web application designed to assist students and/or teachers with creating questions and answers for those questions. Built by a small team using Flask, HTML, CSS, JavaScript, and openAI.',
             '/'
-        ],
-        [
-            'rake', 
-            '../images/rake.png', 
-            'A client and server applications for rake, a protocol for distributed compilation and remote command execution. Completed in a pair using Python and C.',
-            'https://github.com/pavo-etc/rake'
-        ],
-        [
-            'duplicates', 
-            '../images/duplicates.png', 
-            'duplicates is a command line utility to locate and report duplicate files in a named directory based on SHA2 hashes. I completed this project with my project partner for the Systems Programming unit.',
-            'https://github.com/demstar16/CLI-duplicates'
-        ],
-        [
-            'Ridgefield Farm Database', 
-            '../images/ridgefield.png',
-            'A web application I designed and developed in a team of 4, working closely with the client, for Ridgefield Farm UWA to help them manage paddock data. Built using Django and deployed on Heroku.',
-            'https://github.com/demstar16/Ridgefield-Farm-Database'
         ],
     ]
     
