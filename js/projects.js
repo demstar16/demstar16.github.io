e89cba4047e7919f3e3c8a19e0453118472de359
demstar16 <44187160+demstar16@users.noreply.github.com>
2023-10-21 14:09:22 +0800

    update project page

--- js/projects.js
+++ js/projects.js
@@ -20,34 +20,28 @@ document.addEventListener("DOMContentLoaded", function() {
             'https://github.com/demstar16/password-manager'
         ],
         [
-            'Pong Clone', 
-            '../images/pong1.png', 
-            'A Pong clone created in the Harvard CS50 gaming course written in Lua. I did not create the code, I just extended it to include a basic AI you could play along with the 2 player.',
-            'https://github.com/demstar16/pong-clone'
+            'Adam Sandler Wordle', 
+            '../images/adam-sandler-wordle.png',
+            'An Adam Sandler inspired wordle like game created with Flask. One of my favorite projects to date and I learnt a lot making it.',
+            'https://github.com/demstar16/adam-sandler-audio-wordle'
         ],
         [
-            'rake', 
-            '../images/rake1.png', 
-            'A client and server applications for rake, a protocol for distributed compilation and remote command execution. Completed in a pair using Python and C.',
-            'https://github.com/pavo-etc/rake'
-        ],
-        [
-            'duplicates', 
+            'Duplicates CLI', 
             '../images/duplicates1.png', 
             'duplicates is a command line utility to locate and report duplicate files in a named directory based on SHA2 hashes. I completed this project with my project partner for the Systems Programming unit.',
             'https://github.com/demstar16/cli-duplicates'
+        ],
+        [
+            'Rake Network Protocol', 
+            '../images/rake1.png', 
+            'Client and server applications for rake, a protocol for distributed compilation and remote command execution. Completed in a pair using Python and C.',
+            'https://github.com/pavo-etc/rake'
         ],
         [
             'Ridgefield Farm Database', 
             '../images/ridgefield1.png',
             'A web application I designed and developed in a team of 4, working closely with the client, for Ridgefield Farm UWA to help them manage paddock data. Built using Django and deployed on Heroku.',
             'https://github.com/demstar16/ridgefield-farm-database'
-        ],
-        [
-            'Flappy Bird Clone', 
-            '../images/flappy-bird1.png', 
-            'A Flappy Bird clone created in the Harvard CS50 gaming course written in Lua. I did not create all the code but rather added features, these included: pause functionality, rewards depending on score, and randomizing distance between pipe pairs and gap distance.',
-            'https://github.com/demstar16/flappy-bird-clone'
         ],
         [
             'Triumph', 
@@ -60,6 +54,18 @@ document.addEventListener("DOMContentLoaded", function() {
             '../images/examgpt1.png', 
             'A web application designed to assist students and/or teachers with creating questions and answers for those questions. Built by a small team using Flask, HTML, CSS, JavaScript, and openAI.',
             'https://github.com/demstar16/examGPT'
+        ],
+        [
+            'Pong Clone', 
+            '../images/pong1.png', 
+            'A Pong clone created in the Harvard CS50 gaming course written in Lua. I did not create the code, I just extended it to include a basic AI you could play along with the 2 player.',
+            'https://github.com/demstar16/pong-clone'
+        ],
+        [
+            'Flappy Bird Clone', 
+            '../images/flappy-bird1.png', 
+            'A Flappy Bird clone created in the Harvard CS50 gaming course written in Lua. I did not create all the code but rather added features, these included: pause functionality, rewards depending on score, and randomizing distance between pipe pairs and gap distance.',
+            'https://github.com/demstar16/flappy-bird-clone'
         ],
     ]
     
